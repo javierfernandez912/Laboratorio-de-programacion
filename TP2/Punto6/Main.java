a6cb95dd4faef4eeb1f210709c780ac32a52ea05
javierfernandez912 <jesustrevisan5@gmail.com>
2026-04-26 21:03:57 -0300

    Pequeño cambio

--- TP2/Punto6/Main.java
+++ TP2/Punto6/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] args) {
         CalculadorPromedios c1 = new CalculadorPromedios();
         try {
-            c1.promediarAlumnos("TP2/Punto5/Calificaciones.xml", "TP2/Punto6", "promedioDeAlumnos");
+            c1.promediarAlumnos("TP2/Punto5/Calificaciones.xml", "TP2/Punto6", "promedioDeAlumnos"); 
+            //los parametros son
+            //ruta de donde agarra el xml a promediar
+            //ruta donde va a guardar el nuevo archivo
+            //nombre del nuevo archivo
 
         } catch (Exception e) {
             e.printStackTrace();
